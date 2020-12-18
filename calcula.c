45a57c5b29e4b6712e5af83fd721ffb8b8c9d64f
Surabhi Thakur <61082171+surabhi-99@users.noreply.github.com>
2020-12-18 17:47:24 +0530

    Add files via upload

---
+++ calcula.c
@@ -0,0 +1,64 @@
+
+#include<stdio.h>
+
+#include<conio.h>
+
+ 
+
+int main() {
+
+    char operator;
+
+    float num1,num2;
+
+     
+
+    printf("Enter two numbers as operands\n");
+
+    scanf("%f%f", &num1, &num2);
+
+    printf("Enter an arithemetic operator(+-*/)\n");
+
+    scanf("%*c%c",&operator);
+
+ 
+
+    switch(operator) {
+
+        case '+': 
+
+         printf("%.2f + %.2f = %.2f",num1, num2, num1+num2);
+
+         break;
+
+        case '-':
+
+                printf("%.2f - %.2f = %.2f",num1, num2, num1-num2);
+
+                break;
+
+        case '*':
+
+                printf("%.2f * %.2f = %.2f",num1, num2, num1*num2);
+
+                break;
+
+        case '/':
+
+                printf("%.2f / %.2f = %.2f",num1, num2, num1/num2);
+
+                break;
+
+        default: 
+
+                printf("ERROR: Unsupported Operation");
+
+    }
+
+     
+
+    getch();
+
+    return 0;
+
+}
